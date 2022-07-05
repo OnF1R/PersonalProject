7ab70474dfee45ed04217c2d11be54147d02b298
OnF1R <bender987@bk.ru>
2022-07-05 18:15:42 +0300

    Подключение Bootstrap, вывод функций

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-  echo 'Hello World!';
- ?>
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <title>Базовая страница</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Базовые функции в PHP</h1>
+      <?php
+      include 'BasicLogic.php';
+      SumIsInteger(5,2);
+      echo "<br>";
+      SumIsInteger(5,'fff');
+       ?>
+    </div>
+  </body>
+</html>
