062b34a8dcfb2286e79ecc863cbce0c274b7d3f9
OnF1R <bender987@bk.ru>
2022-07-05 23:16:04 +0300

    Страница музыкального плеера

---
+++ AudioPlayer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <?php include 'InitBootstrap.php'; ?>
+    <meta charset="utf-8">
+    <title>Музыкальная страница</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Музыкальный плеер</h1>
+
+    </div>
+  </body>
+</html>
