005c3ef71b5de24389ecaa5f412f8f330d93e60c
OnF1R <bender987@bk.ru>
2022-07-12 22:23:34 +0300

    Изучение существующих констант, создание своих, работа с классом Point

--- index.php
+++ index.php
@@ -36,6 +36,37 @@
         echo "<br>";
         CheckNumber(); // Проверка числа
       ?>
+      <h3>Работа с классом Point</h3>
+      <?php
+        include 'Point.php';
+
+        $firstPoint = new Point;
+        $firstPoint->x=10;
+        $firstPoint->y=34;
+
+        $secondPoint = new Point;
+        $secondPoint->x=3;
+        $secondPoint->y=10;
+
+        $range = sqrt(pow($firstPoint->x - $secondPoint->x,2) + pow($firstPoint->y - $secondPoint->y,2));
+        echo "Расстояние между точками равно = $range";
+       ?>
+      <h3>Константы</h3>
+      <?php
+        for ($i=0; $i < 2; $i++) {
+          if (!defined('BIRTHDAY')) {
+            define('BIRTHDAY', "29.02.2002", true);
+            echo "Константа создалась со значением " . constant('BIRTHDAY') . " <br>";
+          }
+          if (defined('BIRTHDAY')) {
+            echo "Константа уже создана, значение ". constant('BIRTHDAY') ." <br>";
+          }
+        }
+        echo 'Имя файла ' . __DIR__ . ' <br>';
+        echo 'Имя файла ' . __FILE__ . ' <br>';
+        echo 'Номер строки ' . __LINE__ . ' <br>';
+
+       ?>
       <h3>Другие функции</h3>
       <?php
         CheckEmpty(123); // Проверка пустоты строки
