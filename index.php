f60e6ef12e7c7aa3c72719836c4a90060e46caa4
OnF1R <bender987@bk.ru>
2022-07-12 19:04:46 +0300

    Изменение отображение старых и добавление новых функций

--- index.php
+++ index.php
@@ -1,22 +1,49 @@
 <!DOCTYPE html>
 <html lang="ru" dir="ltr">
   <head>
+    <?php
+    include 'InitBootstrap.php';
+    include 'BasicLogic.php';
+    ?>
     <meta charset="utf-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <title>Базовая страница</title>
   </head>
   <body>
     <div class="container">
-      <h1>Базовые функции в PHP</h1>
+      <h1><a href="UploadFileForm.php">Загрузить музыку</a></h1>
+      <h1><a href="AudioPlayer.php">Плеер</a></h1>
+      <h3>Функция сложение простых чисел</h3>
       <?php
-      include 'BasicLogic.php';
-      SumIsInteger(5,2);
-      echo "<br>";
-      SumIsInteger(5,'fff');
-      echo "<br>";
-      ShowArray();
-       ?>
+        SumIsInteger(5,16); // Сумма двух целых чисел
+        echo "<br>";
+        SumIsInteger(); // Сумма двух целых чисел
+        echo "<br>";
+        SumIsInteger(5,'fff'); // Сумма двух целых чисел
+      ?>
+      <h3>Функция вывода массива</h3>
+      <?php
+        ShowArray(); // Сумма двух целых чисел
+        echo "<br>";
+        ShowArray("Жук"); // Массив животных
+      ?>
+      <h3>Функция проверки числа</h3>
+      <?php
+        CheckNumber(55); // Проверка числа
+        echo "<br>";
+        CheckNumber(10); // Проверка числа
+        echo "<br>";
+        CheckNumber(randomInt(100,500)); // Проверка числа
+        echo "<br>";
+        CheckNumber(); // Проверка числа
+      ?>
+      <h3>Другие функции</h3>
+      <?php
+        CheckEmpty(123); // Проверка пустоты строки
+        echo "<br>";
+        CheckType("true"); // Проверка типа переменной
+        echo "<br>";
+        echo Rounded(21.41231231, 4);
+      ?>
     </div>
   </body>
 </html>
