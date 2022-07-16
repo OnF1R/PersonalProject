1bcb8349175248fc22a77c066c948dda302bcbcc
OnF1R <bender987@bk.ru>
2022-07-16 11:42:42 +0300

    switch, while, for

--- BasicLogic.php
+++ BasicLogic.php
@@ -1,5 +1,4 @@
 <?php
-
   function SumIsInteger($a = null,$b = null){
     if (isset($a) && isset($b)) {
       if (is_integer($a) && is_integer($b)) {
@@ -15,7 +14,7 @@
   }
 
   function ShowArray($animal_1 = "Кошка", $animal_2 = "Собака", $animal_3 = "Мышь") {
-    if (is_string($animal_1) && is_string($animal_2) && is_string($animal_3)) {
+    if (is_string($animal_1) & is_string($animal_2) & is_string($animal_3)) {
       $animal_array = $animal_1 . ' ' . $animal_2 . ' ' . $animal_3;
       echo "$animal_array Это строка животных из отдельных переменных";
     }
@@ -26,11 +25,7 @@
 
 
   function CheckEmpty($str = "") {
-    if (empty($str)) {
-      echo "Строка пустая";
-    } else {
-      echo "Содержимое строки - $str";
-    }
+    return empty($str) ? "Строка пустая" : "Содержимое строки - $str";
   }
 
 
@@ -41,7 +36,7 @@
         echo "$number $answer";
     }
     else {
-      $answer = $number == 10 ? "Это десять" : "Вы ввели не числа или не целые числа!";
+      $answer = $number === 10 ? "Это десять" : "Вы ввели не числа или не целые числа!";
       echo "$number $answer";
     }
   } else {
@@ -54,15 +49,49 @@
   }
 
   function CheckType($var = null) {
-    if (isset($var)) {
-      $type = gettype($var);
-      echo "Тип переменной - $type";
-    } else {
-      echo "Вы ничего не ввели!";
+    return isset($var) ? "Тип переменной - " . gettype($var) : "Вы ничего не ввели!";
+  }
+
+  function Rounded($number = null, $pointnumber = null) {
+    return round($number, $pointnumber);
+  }
+
+  function Calc($num1,$num2,$operation) {
+    switch ($operation) {
+      case '+':
+        return $num1 + $num2;
+        break;
+      case '-':
+        return $num1 - $num2;
+        break;
+      case '*':
+        return $num1 * $num2;
+        break;
+      case '/':
+        return $num1 / $num2;
+        break;
+      default:
+        return "Неправильный знак";
+        break;
     }
   }
 
-  function Rounded($number, $pointnumber = null) {
-    return round($number, $pointnumber);
+  function FromOneToNumber ($number = 1) {
+    $count = 0;
+    while ($count < $number ) {
+      $count++;
+      echo "$count\n";
+    }
+    return $number == 0 ? "0 Серьезно?" : "";
+  }
+
+  function FindSimpleNumber($number = 1) {
+    $count = 0;
+    for ($i=2; $i <= $number; $i++) {
+      if ($number % $i == 0) {
+        $count++;
+      }
+    }
+    return $count == 1 ? "Число простое" : "Число плохое";
   }
  ?>
